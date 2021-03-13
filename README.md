6b78f3b7055dff02b37b95acc8db303068fc59c7
Julio Moraes <julio.cesar.ambos@gmail.com>
2021-03-13 16:56:07 -0300

    :heavy_check_mark:challenge completed:hammer:

---
+++ README.md
@@ -0,0 +1,51 @@
+# 🔥 Ignite - Trilha NodeJS
+
+<h3 align="center">
+  Desafio 03: Corrigindo o código
+</h3>
+<img src="public/challenge.jpg" alt="challenge" />
+
+## :rocket: Sobre o desafio
+
+Nesse desafio, temos uma aplicação Node.js que está em processo de desenvolvimento mas que já possui os testes necessários para fazer toda a validação dos requisitos (você não deve mexer nos testes).
+Após algumas alterações no código da aplicação, parte dos testes deixaram de passar e agora só você pode resolver esse problema. Bora lá? 🚀
+
+### :keyboard: Instalação e Execução do Projeto
+
+- Clone este repositório
+
+```
+> git clone {url-do-repositorio}
+```
+
+- Navegue até o diretório principal do projeto
+
+```
+> cd ignite-trabalhando-com-middlewares
+```
+
+- Instale as dependências com o Yarn
+
+```
+yarn
+```
+
+- Rode a suite de testes
+
+```
+yarn test
+```
+
+- Execute o projeto
+
+```
+yarn dev
+```
+
+## :eyeglasses: Instruções sobre o que deve ser feito
+
+Existe uma documentação no Notion, clique **[aqui](https://www.notion.so/Desafio-03-Corrigindo-o-c-digo-c15c8a2e212846039a367cc7b763c6dd)** para acessar!
+
+## :memo: Licença
+
+Esse projeto está sob a licença MIT. Veja o arquivo [LICENSE](https://github.com/git/git-scm.com/blob/master/MIT-LICENSE.txt) para mais detalhes.
--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 const express = require("express");
-
 const { v4: uuid } = require("uuid");
 
 const app = express();
@@ -13,42 +12,50 @@ app.get("/repositories", (request, response) => {
 });
 
 app.post("/repositories", (request, response) => {
-  const { title, url, techs } = request.body
+  const { title, url, techs } = request.body;
 
   const repository = {
     id: uuid(),
     title,
     url,
     techs,
-    likes: 0
+    likes: 0,
   };
 
-  return response.json(repository);
+  repositories.push(repository);
+
+  return response.status(201).json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
   const { id } = request.params;
   const updatedRepository = request.body;
+  delete updatedRepository.likes;
 
-  repositoryIndex = repositories.findindex(repository => repository.id === id);
+  const repositoryIndex = repositories.findIndex(
+    (repository) => repository.id === id
+  );
 
   if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
   }
 
-  const repository = { ...repositories[repositoryIndex], ...updatedRepository };
+  const existingRepository = repositories[repositoryIndex];
 
-  repositories[repositoryIndex] = repository;
+  repositories[repositoryIndex] = {...existingRepository, ...updatedRepository };
 
-  return response.json(repository);
+    
+  return response.json(repositories[repositoryIndex]);
 });
 
 app.delete("/repositories/:id", (request, response) => {
   const { id } = request.params;
 
-  repositoryIndex = repositories.findIndex(repository => repository.id === id);
+  const repositoryIndex = repositories.findIndex(
+    (repository) => repository.id === id
+  );
 
-  if (repositoryIndex > 0) {
+  if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
   }
 
@@ -60,15 +67,17 @@ app.delete("/repositories/:id", (request, response) => {
 app.post("/repositories/:id/like", (request, response) => {
   const { id } = request.params;
 
-  repositoryIndex = repositories.findIndex(repository => repository.id === id);
+  const repositoryIndex = repositories.findIndex(
+    (repository) => repository.id === id
+  );
 
   if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
   }
 
-  const likes = ++repositories[repositoryIndex].likes;
+  repositories[repositoryIndex].likes++
 
-  return response.json('likes');
+  return response.json(repositories[repositoryIndex]);
 });
 
 module.exports = app;
